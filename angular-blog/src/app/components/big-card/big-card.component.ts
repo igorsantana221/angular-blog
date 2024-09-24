e50b46c796640bce92ef57683a022cd1366f21fb
Igor Santana <igorsantana@polimold.com.br>
2024-09-24 17:24:53 -0300

    adicionado itens como variveis

--- angular-blog/src/app/components/big-card/big-card.component.ts
+++ angular-blog/src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
 
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = "Marvel Studio anuncia mais novo movie para o cinema no brasil."
+
+    ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
